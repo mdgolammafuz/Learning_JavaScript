52d214f2149afcf9bc6b9a101de6b9c7aae5d18b
mdgolammafuz <golammafuzgm@example.com>
2022-10-04 03:28:52 +0530

    Primitive / Value Types

---
+++ index.js
@@ -0,0 +1,17 @@
+let name;
+console.log(name) // We will get undefined in the console as it is not initialized
+
+let anotherName = 'Mafuz!'; //  String Literal. We can use "" instead of ''
+console.log(anotherName)
+
+let age = 30;
+console.log(age)
+
+let isDead = false;
+console.log(isDead)
+
+let firstName = undefined;
+console.log(firstName)
+
+let selectedLanguage = null; // when we want to clear the value of a variable
+console.log(selectedLanguage)
