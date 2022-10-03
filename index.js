c2ad67d4a21e9eef37c2ae8a35727bcabadacc15
mdgolammafuz <golammafuzgm@example.com>
2022-10-04 05:03:08 +0530

    Dynamic Programming

--- index.js
+++ index.js
@@ -4,10 +4,15 @@ console.log(name) // We will get undefined in the console as it is not initializ
 let anotherName = 'Mafuz!'; //  String Literal. We can use "" instead of ''
 console.log(anotherName)
 
-let age = 30;
+let age = 30; // Intergers & Floating point numbers, both are of number type
 console.log(age)
+console.log(typeof age) // 
 
-let isDead = false;
+age = "Thirty"; // We are changing the type of the value of the variable
+console.log(age)
+console.log(typeof age)
+
+let isDead = false; // Boolean
 console.log(isDead)
 
 let firstName = undefined;
@@ -15,3 +20,9 @@ console.log(firstName)
 
 let selectedLanguage = null; // when we want to clear the value of a variable
 console.log(selectedLanguage)
+
+console.log(typeof firstName)
+
+console.log(typeof isDead)
+
+console.log(typeof selectedLanguage)
