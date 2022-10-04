932b9d48617da57974abcc09e5a9f5be58c406f2
mdgolammafuz <golammafuzgm@example.com>
2022-10-04 06:14:11 +0530

    Array

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 let name;
-console.log(name) // We will get undefined in the console as it is not initialized
+console.log(name) /* We will get undefined in the 
+                     console as it is not initialized */
 
-let anotherName = 'Mafuz!'; //  String Literal. We can use "" instead of ''
+let anotherName = 'Mafuz!'; // String Literal. We can use "" instead of ''
 console.log(anotherName)
 
 let age = 30; // Intergers & Floating point numbers, both are of number type
@@ -41,3 +42,12 @@ let selection = 'name'; // Usuallu used when we don't know the target property b
 person[selection] = "Nasrina";
 
 console.log(person.name)
+
+let selectedProducts = ['Book', 'Laptop', 'Battery'];
+
+selectedProducts[3] = 1; /* length of an Array can change. The type of objects in an Array
+                                 can also change */
+
+console.log(selectedProducts)
+console.log(typeof selectedProducts) // Type of an array is Object
+console.log(typeof selectedProducts.length) // Array objects have built in properties 
