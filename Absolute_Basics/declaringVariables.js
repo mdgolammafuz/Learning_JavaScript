55868d23fa14ca67670553234eedc96bc96a6123
mdmafuz2022 <golam.mafuz.work2@gmail.com>
2022-12-14 05:30:32 +0530

    Declaring Variables

---
+++ Absolute_Basics/declaringVariables.js
@@ -0,0 +1,17 @@
+let cookiesInJar = 10;
+ 
+/* eat two cookies */
+cookiesInJar -= 2;  // leaves 8 cookies
+console.log(cookiesInJar)
+ 
+/* bake a new batch of cookies */
+cookiesInJar += 12; // there are now 20 cookies in the jar
+console.log(cookiesInJar)
+
+let numberOfLikes = 10;
+ 
+numberOfLikes++;  // makes 11
+console.log(numberOfLikes)
+ 
+numberOfLikes--; // and back to 10...who disliked my post??
+console.log(numberOfLikes)
